bba49c92d9c52c3a5e8e09efc30f67f71c60fe07
yogeshmo <yogeshmo@umich.edu>
2018-05-17 22:40:50 -0400

    adds image and button on timeline

--- Scripts/projects_scripts.js
+++ Scripts/projects_scripts.js
@@ -10,6 +10,92 @@ function select_card(card_num) {
   cards[card_num - 1].className += " card" + card_num.toString();
 }
 
+
+// Adds image of the project to selected card
+function add_image(card_num) {
+  // removes all images from previous selected cards
+  var prev_img = document.getElementsByClassName("project_picture");
+
+  if (prev_img.length != 0) {
+    prev_img[0].parentNode.removeChild(prev_img[0]);
+  }
+
+  var img = document.createElement("img");
+  img.className = "project_picture";
+  img.src = "../Images/project_picture" + card_num.toString() + ".png";
+
+  var current_card = document.getElementsByClassName("card" + card_num.toString());
+  current_card[0].appendChild(img);
+}
+
+
+// sets and manages button links and text
+function set_button(button, card_num) {
+  button.className = "project_button";
+  if (card_num == 1) {
+    button.onclick = 
+      function() { 
+        window.open("https://github.com/yogeshmohanraj/PortfolioWebsite"); 
+      };
+    button.innerHTML = "GitHub";
+  }
+  else if (card_num == 2) {
+    button.onclick = 
+      function() { 
+          window.open("../Pages/contact.html"); 
+        };
+    button.innerHTML = "Request Source Code";
+  }
+  else if (card_num == 3) {
+    button.onclick = 
+      function() { 
+          window.open("../Pages/contact.html"); 
+        };
+    button.innerHTML = "Request Source Code";
+  }
+  else if (card_num == 4) {
+    button.onclick = 
+      function() { 
+          window.open("../Pages/contact.html"); 
+        };
+    button.innerHTML = "Request Source Code";
+  }
+  else if (card_num == 5) {
+    button.onclick = 
+      function() { 
+          window.open("../Pages/contact.html"); 
+        };
+    button.innerHTML = "Request Source Code";
+  }
+  else if (card_num == 6) {
+    button.onclick = 
+      function() { 
+          window.open("https://drive.google.com/drive/folders/1ab4lvN4Egvc9PTWO0A4aN9lczBNP96Jp?usp=sharing"); 
+        };
+    button.innerHTML = "Team Drive Folder";
+  }
+}
+
+
+// Adds button to card
+function add_button(card_num) {
+  // removes all buttons from previous selected cards
+  var prev_button = document.getElementsByClassName("project_button");
+
+  if (prev_button.length != 0) {
+    prev_button[0].parentNode.removeChild(prev_button[0]);
+  }
+
+  var button = document.createElement("button");
+  set_button(button, card_num);
+
+  var current_card = document.getElementsByClassName("card" + card_num.toString());
+  current_card[0].appendChild(button);
+}
+
+
+
+
 // Determines if object is visible, the adds the "in-view class"
 var items = document.querySelectorAll(".timeline li");
 
